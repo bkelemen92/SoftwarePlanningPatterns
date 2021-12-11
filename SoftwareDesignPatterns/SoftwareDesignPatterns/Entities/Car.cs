baac1cf1760b92bdf2269e3c9687d4de1d1094e9
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-12-11 11:21:25 +0100

    Created Car class

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using SoftwareDesignPatterns.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            graphics.DrawImage(Image.FromFile("Images/car.png"), new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
