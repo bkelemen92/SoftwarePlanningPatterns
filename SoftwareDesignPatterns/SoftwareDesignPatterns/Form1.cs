ee3f99c17664220e603fa993806d316dd28b522c
Kelemen Balázs <kelemen.balazs@andoc.net>
2021-12-11 11:27:00 +0100

    Created Factory Interface

--- SoftwareDesignPatterns/SoftwareDesignPatterns/Form1.cs
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/Form1.cs
@@ -41,7 +41,7 @@ namespace SoftwareDesignPatterns
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition) maxPosition = ball.Left;
             }
             if(maxPosition > mainPanel.Width)
